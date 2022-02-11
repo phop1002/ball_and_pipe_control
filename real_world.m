a8ca4834e31cac0534a9098a980c29e983985041
Whitten Oswald <whittenoswald@gmail.com>
2022-02-11 09:14:05 -0500

    changed comport

--- real_world.m
+++ real_world.m
@@ -3,34 +3,34 @@
 % The current control system is a PID controller.
 %
 % Created by Kyle Naddeo, Mon Jan 3 11:19:49 EST 
-% Modified by YOUR NAME AND DATE
+% Modified by Whitten Oswald on February 11, 2022
 
 %% Start fresh
 close all; clc; clear device;
 
 %% Connect to device
-% device = open serial communication in the proper COM port
+device = serialport(COM3,19200);
 
 %% Parameters
 target      = 0.5;   % Desired height of the ball [m]
 sample_rate = 0.25;  % Amount of time between controll actions [s]
 
 %% Give an initial burst to lift ball and keep in air
-% set_pwm(add_proper_args); % Initial burst to pick up ball
+set_pwm(device,2500); % Initial burst to pick up ball
 pause(0.1) % Wait 0.1 seconds
-% set_pwm(add_proper_args); % Set to lesser value to level out somewhere in
+set_pwm(device,1500); % Set to lesser value to level out somewhere in
 % the pipe
 
 %% Initialize variables
-% action      = ; % Same value of last set_pwm   
+action      = 1500; % Same value of last set_pwm   
 error       = 0;
 error_sum   = 0;
 
 %% Feedback loop
 while true
     %% Read current height
-    % [add_proper_args] = read_data(add_proper_args);
-    % y = ir2y(add_proper_args); % Convert from IR reading to distance from bottom [m]
+    [distance,pwm,target,deadpan] = read_data(device);
+    y = ir2y(add_proper_args); % Convert from IR reading to distance from bottom [m]
     
     %% Calculate errors for PID controller
     error_prev = error;             % D
