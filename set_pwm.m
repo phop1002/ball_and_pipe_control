d2f8abb7960a8bfcbf1098682fe007bdede507ba
Whitten Oswald <whittenoswald@gmail.com>
2022-02-18 10:03:07 -0500

    hey

--- set_pwm.m
+++ set_pwm.m
@@ -8,13 +8,26 @@ function action = set_pwm(device, pwm_value)
 %  ~ action: the control action to change the PWM
 %
 % Created by:  Kyle Naddeo 2/4/2022
-% Modified by: YOUR NAME and DATE
+% Modified by: Whitten Oswald on 2/18/2022
 
 %% Force PWM value to be valid
-% pwm_value = % Bound value to limits 0 to 4095
+% Bound value to limits 0 to 4095
+
+UpperBound=4095;
+LowerBound=0;
+
+if (pwm_value > UpperBound)
+    pwm_value = UpperBound;
+elseif (pwm_value < LowerBound)
+    pwm_value = LowerBound;
+else
+    pwm_value = pwm_value;
+end
+
 
 %% Send Command
-% action = % string value of pwm_value
+action = sprintf('P%04.f', pwm_value);
+% string value of pwm_value
 % use the serialport() command options to change the PWM value to action
 
 end
